6242fa96700315a092840e3a50b4942a48a4d098
Lain <34698781+YLQY@users.noreply.github.com>
2023-09-04 21:54:42 +0800

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,1 +1,14 @@
+apt install git-lfs
+pip install transformers==4.32.0
+pip install datasets
+pip install transformers[torch]
+pip install tensorboardX
+pip install torch
+pip install torchaudio
+pip install librosa
+pip install jiwer
+pip install evaluate
+pip install -U requests[security]
 
+# 这个命令可能一次运行不成功，尝试多运行几次
+pip install -q git+https://github.com/huggingface/peft.git@main
