46b8e0059b46e751b1cfbac9c2e82c09958e21d4
Lain <34698781+YLQY@users.noreply.github.com>
2023-09-04 23:00:44 +0800

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,14 +1,14 @@
 apt install git-lfs
-pip install transformers==4.32.0
-pip install datasets
-pip install transformers[torch]
-pip install tensorboardX
-pip install torch
-pip install torchaudio
-pip install librosa
-pip install jiwer
-pip install evaluate
-pip install -U requests[security]
+pip install transformers==4.32.0 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install datasets -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install transformers[torch] -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install tensorboardX -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install torch -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install torchaudio -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install librosa -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install jiwer -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install evaluate -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -U requests[security] -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # 这个命令可能一次运行不成功，尝试多运行几次
 pip install -q git+https://github.com/huggingface/peft.git@main
